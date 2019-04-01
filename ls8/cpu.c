e2e58ca63841cf7e3d1fd60d76333700ec6300ee
Matthew Wright <matthewbwright@gmail.com>
2019-04-01 16:05:39 -0700

    implements cpu_init

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -67,6 +67,9 @@ void cpu_run(struct cpu *cpu)
 void cpu_init(struct cpu *cpu)
 {
   // TODO: Initialize the PC and other special registers
+  cpu->PC = 0;
+  memset(cpu->ram, 0, 8 * sizeof(unsigned char));
+  memset(cpu->registers, 0, 256 * sizeof(unsigned char));
 }
 
 void cpu_ram_read()
